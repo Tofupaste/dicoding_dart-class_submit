15babfbb24b64e6656408fced139dbb34bab05f3
Aria <abiputra3566@gmail.com>
2024-12-18 22:03:22 +0700

    submit_d1

---
+++ android/app/src/main/kotlin/com/example/dicoding_submit/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.dicoding_submit
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ lib/main.dart
@@ -0,0 +1,31 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'MYIstri Apps',
+      theme: ThemeData(),
+      home: const DetailScreen(),
+    );
+  }
+}
+
+class DetailScreen extends StatelessWidget {
+  const DetailScreen({Key? key}) : super(key: key);
+
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Column(),
+    )
+  }
+}
