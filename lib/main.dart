02ec67fddb27810ca538e289f93b741a8c501539
Aria <abiputra3566@gmail.com>
2024-12-19 21:33:09 +0700

    updates_3

--- lib/main.dart
+++ lib/main.dart
@@ -25,13 +25,20 @@ class DetailScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Column(
-        children: <Widget> [
-          Container(
-            margin: const EdgeInsets.only(top: 16.0),
-            child:Text('Saki Tenma')),
-        ],
-      ),
+      body: SafeArea(
+        child: Column(
+          children: <Widget> [
+            Container(
+              margin: const EdgeInsets.only(top: 16.0),
+              child:const Text('Saki Tenma', 
+                textAlign: TextAlign.center, 
+                style: TextStyle(
+                  fontSize: 30.0, fontWeight: FontWeight.bold,
+                ),)
+              )
+            ],
+          ),
+        )
     );
   }
 }
