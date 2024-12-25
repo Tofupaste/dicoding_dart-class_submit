e1f3bc8e61bf7caa7e8f87d87ea537cc7cd1c689
Aria <abiputra3566@gmail.com>
2024-12-26 02:41:07 +0700

    backup for sane2

---
+++ lib/model/istris.dart
@@ -0,0 +1,47 @@
+// istris.dart
+import 'dart:ui';
+
+class Istris {
+  String name;
+  String umur;
+  String band;
+  VoidCallback voice;
+  String Function() biodata;
+  String imageAsset;
+  List<String> imageUrls;
+
+  Istris({
+    required this.name,
+    required this.umur,
+    required this.band,
+    required this.voice,
+    required this.biodata,
+    required this.imageAsset,
+    required this.imageUrls,
+  });
+}
+
+var IstrisList = [
+  Istris(
+    name: 'Saki Tenma',
+    umur: '17',
+    band: 'Leo/need',
+    voice: () {
+      // Code to play the audio file
+      playVoice('audio/saki_1.wav');
+    },
+    biodata: () =>
+        'Saki Tenma is a cheerful and kind character from the game Project Sekai.',
+    imageAsset: 'images/saki_bg.png',
+    imageUrls: [
+      'https://static.wikia.nocookie.net/projectsekai/images/6/63/The_Best_Doll_Festival%21.png',
+      'https://static.wikia.nocookie.net/projectsekai/images/6/63/The_Best_Doll_Festival%21.png',
+      'https://static.wikia.nocookie.net/projectsekai/images/6/63/The_Best_Doll_Festival%21.png',
+    ],
+  ),
+];
+
+void playVoice(String path) {
+  // Implement the function to play the audio file
+  print('Playing voice from $path');
+}
