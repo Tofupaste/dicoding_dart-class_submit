d2f18e1a109ab11a7b59e01ff1b2d6c54e1c458d
Aria <abiputra3566@gmail.com>
2024-12-25 19:34:50 +0700

    fixing_main2

--- lib/main_screen.dart
+++ lib/main_screen.dart
@@ -1,44 +1,52 @@
+import 'package:dicoding_submit/main.dart';
 import 'package:flutter/material.dart';
+import 'detail_screen.dart'; 
 
 class MainScreen extends StatelessWidget {
-  const MainScreen ({super.key});
-
+  const MainScreen({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text('MYIstri Apps'),
+        title: const Text('Wisata Bandung'),
       ),
-      body: Card(
-        child: Row(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: <Widget> [
-            Expanded(
-              flex: 1,
-              child: Image.asset('images/saki_bg.png'),
-            ),
-            Expanded(
-              flex: 2,
-              child: Padding(
-                padding: const EdgeInsets.all(8.0),
-                child: Column(
-                  crossAxisAlignment:CrossAxisAlignment.start,
-                  mainAxisSize: MainAxisSize.min,
-                  children: <Widget>[
-                    const Text(
-                      'Saki Tenma',
-                      style: TextStyle(fontSize: 16.0),
+      body: InkWell(
+        onTap: () {
+          Navigator.push(context, MaterialPageRoute(builder: (context) {
+            return const DetailScreen();
+          }));
+        },
+        child: Card(
+          child: Row(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: <Widget>[
+              Expanded(
+                flex: 1,
+                child: Image.asset('images/saki_bg.png'),
+              ),
+              Expanded(
+                flex: 2,
+                child: Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    mainAxisSize: MainAxisSize.min,
+                    children: <Widget>[
+                      const Text(
+                        'Saki Tenma',
+                        style: TextStyle(fontSize: 16.0),
                       ),
                       const SizedBox(
-                        height: 10
-                        ),
-                        const Text('Saki')
-                  ],
+                        height: 10,
+                      ),
+                      const Text('Leo/Need'),
+                    ],
                   ),
+                ),
               ),
-            ),
-          ],
+            ],
+          ),
         ),
       ),
     );
