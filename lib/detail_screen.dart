d61b789063876e31f361166f2a3883004bcf55c8
Aria <abiputra3566@gmail.com>
2024-12-26 02:38:17 +0700

    backup for sane

--- lib/detail_screen.dart
+++ lib/detail_screen.dart
@@ -1,4 +1,2 @@
 import 'package:flutter/material.dart';
 import 'package:dicoding_submit/detail_screen.dart';
-
-
--- lib/main_screen.dart
+++ lib/main_screen.dart
@@ -1,54 +1,54 @@
-import 'package:dicoding_submit/main.dart';
 import 'package:flutter/material.dart';
-import 'detail_screen.dart'; 
+import 'package:dicoding_submit/main.dart';
 
 class MainScreen extends StatelessWidget {
-  const MainScreen({Key? key}) : super(key: key);
+  const MainScreen ({super.key});
+
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text('Wisata Bandung'),
+        title: const Text('MYIstri Apps'),
       ),
       body: InkWell(
         onTap: () {
-          Navigator.push(context, MaterialPageRoute(builder: (context) {
+          Navigator.push(context, MaterialPageRoute(builder:(context) {
             return const DetailScreen();
-          }));
+          },));
         },
         child: Card(
-          child: Row(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: <Widget>[
-              Expanded(
-                flex: 1,
-                child: Image.asset('images/saki_bg.png'),
-              ),
-              Expanded(
-                flex: 2,
-                child: Padding(
-                  padding: const EdgeInsets.all(8.0),
-                  child: Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    mainAxisSize: MainAxisSize.min,
-                    children: <Widget>[
-                      const Text(
-                        'Saki Tenma',
-                        style: TextStyle(fontSize: 16.0),
+        child: Row(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: <Widget> [
+            Expanded(
+              flex: 1,
+              child: Image.asset('images/saki_bg.png'),
+            ),
+            Expanded(
+              flex: 2,
+              child: Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: const Column(
+                  crossAxisAlignment:CrossAxisAlignment.start,
+                  mainAxisSize: MainAxisSize.min,
+                  children: <Widget>[
+                    Text(
+                      'Saki Tenma',
+                      style: TextStyle(fontSize: 16.0),
                       ),
-                      const SizedBox(
-                        height: 10,
-                      ),
-                      const Text('Leo/Need'),
-                    ],
+                      SizedBox(
+                        height: 10
+                        ),
+                        Text('Saki')
+                  ],
                   ),
-                ),
               ),
-            ],
-          ),
+            ),
+          ],
         ),
       ),
+    )
     );
   }
 }
